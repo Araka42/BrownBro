19c2c8e3b0f81db78bcf89d1c2890b4586ef23ef
Raphael Grelon <gh api raphaelgrelon@outlook.fr>
2022-03-31 16:47:07 +0200

    routes + static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def nosmetiers
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/nosmetiers", to: "pages#nosmetiers", as: "nosmetiers"
+  get "/contact", to: "pages#contact", as: "contact"
+
+  resources :projects do
+    resources :jointures, only: [:create, :update, :destroy]
+  end
+  resources :categories, only: [:index, :create, :destroy, :new]
 end
