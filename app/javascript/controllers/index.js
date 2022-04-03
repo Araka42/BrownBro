50038ab59337f52a7c3fcd7d7607e8d57e390cf9
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-03 18:24:00 +0200

    pb stimulus/js résolu

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,10 +3,10 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-import { ScrollTo } from 'stimulus-scroll-to'
+// import { ScrollTo } from 'stimulus-scroll-to'
 
 
 const application = Application.start()
+// application.register("scroll-to", ScrollTo)
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
-application.register("scroll-to", ScrollTo)
