576e32980fe0c7f9c2222a34154cbdc07e959b28
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-03 15:08:24 +0200

    scroll réussi

---
+++ app/javascript/controllers/scroll_to_controller.js
@@ -0,0 +1,12 @@
+// import { Controller } from "stimulus"
+
+// export default class extends Controller {
+//   static targets = ["job"]
+
+
+//   scroll(click) {
+//     let _1vh = Math.round(window.innerHeight / 100)
+//     console.log("hello")
+//     window.scroll(0, _1vh)
+//   }
+// }
