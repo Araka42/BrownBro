a3edc3b521d8443fd9ec61bbd2abdc9f3fd7040a
Myriam Delbreil <myriam.delbreil@live.fr>
2022-05-03 10:09:15 +0200

    rajout d'une ligne commentée pour pouvoir push

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,3 +46,5 @@ class ProjectsController < ApplicationController
     params.require(:project).permit(:title, :description, :user_id, :video_url, :user_id, :created_at, :updated_at)
   end
 end
+
+#test
