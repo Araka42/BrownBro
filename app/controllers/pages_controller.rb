0b9129bb92f0270124e8d43ee47fc2d72cd15ed2
Myriam Delbreil <myriam.delbreil@live.fr>
2022-05-04 13:07:48 +0200

    skip login ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def home
   end
 
   def nosmetiers
-  end
-
-  def contact
   end
 
 end
