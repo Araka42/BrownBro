2198cca3805bd3d3b837ec0ff1cb2db3e43f89e8
Myriam Delbreil <myriam.delbreil@live.fr>
2022-05-04 11:52:22 +0200

    à annuler

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
   resources :categories, only: [:index, :create, :destroy, :new]
   resources :contacts, only: [:new, :create]
+  resources :messages, only: [:new, :create]
 end
